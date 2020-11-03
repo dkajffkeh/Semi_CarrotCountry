de90a98ed03d422cdb758649d930852715d0da8f
lsh7206 <lsh7206@daum.net>
2020-11-04 04:41:15 +0900

    commit

--- CarrotCountry/src/com/javachip/carrotcountry/jmboard/notice/model/dao/NoticeDao.java
+++ CarrotCountry/src/com/javachip/carrotcountry/jmboard/notice/model/dao/NoticeDao.java
@@ -1,44 +1,33 @@
 package com.javachip.carrotcountry.jmboard.notice.model.dao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 import static com.javachip.carrotcountry.common.JDBCtemplate.*;
 public class NoticeDao {
 	
 	private Properties prop = new Properties();
 	
-	
-	
-
-	public int selectListCount(Connection conn) {
-		
-		int listCount = 0;
-		
-		Statement stmt = null;
-		ResultSet rset = null;
-		
-		String sql = prop.getProperty("selectListCount");
+	public NoticeDao() {
 		
 		try {
-			stmt = conn.createStatement();
-			
-			rset = stmt.executeQuery(sql);
-			
-			if(rset.next()) {
-				listCount = rset.getInt("LISTCOUNT");
-			}
-			
-		} catch (SQLException e) {
+			prop.loadFromXML(new FileInputStream(NoticeDao.class.getResource("/sql/jmboard/notice-mapper.xml").getPath()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			close(rset);
-			close(stmt);
 		}
 		
-		return listCount;
+		
 	}
+	
+
+
+
 
 }
--- CarrotCountry/src/com/javachip/carrotcountry/jmboard/notice/model/vo/Notice.java
+++ CarrotCountry/src/com/javachip/carrotcountry/jmboard/notice/model/vo/Notice.java
@@ -1,5 +1,155 @@
 package com.javachip.carrotcountry.jmboard.notice.model.vo;
 
-public class Notice {
+import java.util.Date;
 
+public class Notice {
+	
+	private int noticeNo;
+	private String memNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private String filePath;
+	private Date noticeEnrollDate;
+	private int noticeCount;
+	private String fileModifyName;
+	private String fileOriginName;
+	private String noticeStatus;
+	
+	public Notice() {}
+
+	public Notice(int noticeNo, String memNo, String noticeTitle, String noticeContent, int noticeCount) {
+		super();
+		this.noticeNo = noticeNo;
+		this.memNo = memNo;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.noticeCount = noticeCount;
+	}
+
+	public Notice(int noticeNo, String memNo, String noticeTitle, String noticeContent, String filePath,
+			String fileModifyName, String fileOriginName) {
+		super();
+		this.noticeNo = noticeNo;
+		this.memNo = memNo;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.filePath = filePath;
+		this.fileModifyName = fileModifyName;
+		this.fileOriginName = fileOriginName;
+	}
+
+	public Notice(int noticeNo, String memNo, String noticeTitle, String noticeContent, String filePath,
+			Date noticeEnrollDate, String fileModifyName, String fileOriginName) {
+		super();
+		this.noticeNo = noticeNo;
+		this.memNo = memNo;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.filePath = filePath;
+		this.noticeEnrollDate = noticeEnrollDate;
+		this.fileModifyName = fileModifyName;
+		this.fileOriginName = fileOriginName;
+	}
+
+	public Notice(int noticeNo, String noticeTitle, String noticeContent, String filePath, String fileModifyName,
+			String fileOriginName) {
+		super();
+		this.noticeNo = noticeNo;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.filePath = filePath;
+		this.fileModifyName = fileModifyName;
+		this.fileOriginName = fileOriginName;
+	}
+
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+
+	public String getMemNo() {
+		return memNo;
+	}
+
+	public void setMemNo(String memNo) {
+		this.memNo = memNo;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+
+	public String getNoticeContent() {
+		return noticeContent;
+	}
+
+	public void setNoticeContent(String noticeContent) {
+		this.noticeContent = noticeContent;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Date getNoticeEnrollDate() {
+		return noticeEnrollDate;
+	}
+
+	public void setNoticeEnrollDate(Date noticeEnrollDate) {
+		this.noticeEnrollDate = noticeEnrollDate;
+	}
+
+	public int getNoticeCount() {
+		return noticeCount;
+	}
+
+	public void setNoticeCount(int noticeCount) {
+		this.noticeCount = noticeCount;
+	}
+
+	public String getFileModifyName() {
+		return fileModifyName;
+	}
+
+	public void setFileModifyName(String fileModifyName) {
+		this.fileModifyName = fileModifyName;
+	}
+
+	public String getFileOriginName() {
+		return fileOriginName;
+	}
+
+	public void setFileOriginName(String fileOriginName) {
+		this.fileOriginName = fileOriginName;
+	}
+
+	public String getNoticeStatus() {
+		return noticeStatus;
+	}
+
+	public void setNoticeStatus(String noticeStatus) {
+		this.noticeStatus = noticeStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [noticeNo=" + noticeNo + ", memNo=" + memNo + ", noticeTitle=" + noticeTitle + ", noticeContent="
+				+ noticeContent + ", filePath=" + filePath + ", noticeEnrollDate=" + noticeEnrollDate + ", noticeCount="
+				+ noticeCount + ", fileModifyName=" + fileModifyName + ", fileOriginName=" + fileOriginName
+				+ ", noticeStatus=" + noticeStatus + "]";
+	}
+	
+	
+	
 }
