aa680383cf9ab709bf4ef3aac682d1b08042cab8
PatrickYoo <dkajffkem@gmail.com>
2020-11-09 20:18:00 +0900

    done

--- CarrotCountry/src/com/javachip/carrotcountry/shMarketBoard/mainPage/model/service/TownMarketIndexService.java
+++ CarrotCountry/src/com/javachip/carrotcountry/shMarketBoard/mainPage/model/service/TownMarketIndexService.java
@@ -1,5 +1,24 @@
 package com.javachip.carrotcountry.shMarketBoard.mainPage.model.service;
+import static com.javachip.carrotcountry.common.JDBCtemplate.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.javachip.carrotcountry.shMarketBoard.mainPage.model.dao.TownMarketIndexDao;
+import com.javachip.carrotcountry.shMarketBoard.mainPage.model.vo.PostBoard;
 
 public class TownMarketIndexService {
+
+	public ArrayList<PostBoard> shMarketBoardAjaxSelector() {
+
+		Connection conn = getConnection();
+		
+		ArrayList<PostBoard> pList = new TownMarketIndexDao().shMarketBoardAjaxSelector(conn);
+		
+		close(conn);
+		
+		return pList;
+	}
+
 
 }
