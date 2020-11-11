630d30052955d3a60e88ef5f8fbebc9220dbcd83
allofdev <skarb9511@naver.com>
2020-11-12 00:30:24 +0900

    배송하기

--- CarrotCountry/src/com/javachip/carrotcountry/userinfoBoardNg/controller/CoBuyingBuyerListController.java
+++ CarrotCountry/src/com/javachip/carrotcountry/userinfoBoardNg/controller/CoBuyingBuyerListController.java
@@ -14,16 +14,16 @@ import com.javachip.carrotcountry.userinfoBoardNg.model.vo.MyPurchaseInfo;
 import com.javachip.carrotcountry.userinfoBoardNg.model.vo.RepPageInfo;
 
 /**
- * Servlet implementation class MyGroupPurchaseListController
+ * Servlet implementation class CoBuyingBuyerListController
  */
-@WebServlet("/groupPurchaseList.gp.ng")
-public class MyGroupPurchaseListController extends HttpServlet {
+@WebServlet("/buyerList.co.ng")
+public class CoBuyingBuyerListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyGroupPurchaseListController() {
+    public CoBuyingBuyerListController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,20 +33,20 @@ public class MyGroupPurchaseListController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int memNo = Integer.parseInt(request.getParameter("memNo"));
-			
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		
 		// 페이징 처리 --> RepPageInfo 사용
 		int repListCount;	// 공동구매 총 갯수
 		int repCurrentPage;	// 현재 페이지
 		int repPageLimit;	// 하단에 보여질 페이지 최대 갯수(5)
-		int repBoardLimit; 	// 한 페이지에 보열질 공동구매 최대 갯수(6)
+		int repBoardLimit; 	// 한 페이지에 보열질 공동구매 최대 갯수(10)
 		
 		int repMaxPage;		// 가장 마지막 페이지
 		int repStartPage;	// 페이징바의 시작페이지
 		int repEndPage;		// 페이징바의 끝페이지
 		
-		// 나의 공동구매 총 갯수
-		repListCount = new UserInfoBoardServiceNg().selectPurchaseCount(memNo);	
+		// 나의 공동구매 구매한 회원 리스트 총 수
+		repListCount = new UserInfoBoardServiceNg().buyerListCount(bno);	
 		
 		// 현재 페이지
 		repCurrentPage = Integer.parseInt(request.getParameter("repCurrentPage"));
@@ -55,7 +55,7 @@ public class MyGroupPurchaseListController extends HttpServlet {
 		repPageLimit = 5;
 		
 		// 한페이지 게시글 최대 갯수
-		repBoardLimit = 4;
+		repBoardLimit = 10;
 		
 		// 가장 마지막수
 		repMaxPage = (int)Math.ceil((double)repListCount / repBoardLimit);
@@ -73,12 +73,12 @@ public class MyGroupPurchaseListController extends HttpServlet {
 		RepPageInfo pi = new RepPageInfo(repListCount, repCurrentPage, repPageLimit, 
 				 repBoardLimit, repMaxPage, repStartPage, repEndPage);
 		
-		ArrayList<MyPurchaseInfo> list = new UserInfoBoardServiceNg().selectPurchaseList(pi, memNo);
-		
+		ArrayList<MyPurchaseInfo> list = new UserInfoBoardServiceNg().buyerList(pi, bno);
+
 		request.setAttribute("pi", pi);
 		request.setAttribute("list", list);
 		
-		request.getRequestDispatcher("views/userinfoBoard/ng/myGroupPurchaseListView.jsp").forward(request, response);
+		request.getRequestDispatcher("views/userinfoBoard/ng/coBuyingBuyerList.jsp").forward(request, response);
 		
 	}
 
@@ -91,3 +91,14 @@ public class MyGroupPurchaseListController extends HttpServlet {
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
